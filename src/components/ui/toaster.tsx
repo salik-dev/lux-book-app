9f482557151c28ab62ff1675656f0ec2264b0bdf
salik-dev <balisaalik@gmail.com>
2025-09-25 01:32:42 -0700

    'update'

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -1,6 +1,5 @@
-'use client'
-
-import { useToast } from '@/hooks/use-toast'
+import React from 'react'
+import { useToast } from '../../hooks/use-toast'
 import {
   Toast,
   ToastClose,
@@ -8,7 +7,7 @@ import {
   ToastProvider,
   ToastTitle,
   ToastViewport,
-} from '@/components/ui/toast'
+} from './toast'
 
 export function Toaster() {
   const { toasts } = useToast()
