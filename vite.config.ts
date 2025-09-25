e48194fbe0a632ddf3e7cfad66357a0574a53c21
salik-dev <balisaalik@gmail.com>
2025-09-25 02:48:55 -0700

    'update'

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/lux-book-app/' : '/',
   plugins: [react()],
   css: {
     postcss: {
       plugins: [
         require('@tailwindcss/postcss'),
-        require('autoprefixer'),
       ],
     },
   },
