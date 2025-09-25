1fbc84a0f2b8493132485f256ddff6f52f09cbde
salik-dev <balisaalik@gmail.com>
2025-09-25 03:02:45 -0700

    'update'

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/lux-book-app/' : '/',
+  // base: process.env.NODE_ENV === 'production' ? '/lux-book-app/' : '/',
   plugins: [react()],
   css: {
     postcss: {
