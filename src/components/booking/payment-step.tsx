3b55ee77f72317775836ccd733383534cae56ce1
salik-dev <balisaalik@gmail.com>
2025-10-17 10:58:56 +0500

    'update'

--- src/components/booking/payment-step.tsx
+++ src/components/booking/payment-step.tsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Button } from "../ui/button";
-import {
-  Card,
+import { Card,
   CardContent,
   CardHeader,
   CardTitle,
