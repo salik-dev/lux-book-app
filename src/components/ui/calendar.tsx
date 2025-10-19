c4918a53f5664662f0040e5ef5bb7c1e0f824047
salik-dev <balisaalik@gmail.com>
2025-10-20 03:36:18 +0500

    'update'

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,10 +1,7 @@
 "use client";
 
 import * as React from "react";
-import {
-  ChevronLeft,
-  ChevronRight,
-} from "lucide-react";
+import { ChevronLeft,ChevronRight } from "lucide-react";
 import { DayPicker } from "react-day-picker";
 
 import { buttonVariants } from "./button";
